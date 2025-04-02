5fa32671613fc64d037d2474322eebb0ef56d058
notaspirit <unlequitbaum@gmail.com>
2025-04-02 12:45:18 +0200

    initial commit

---
+++ YOUR_APPNAME/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.CommandLine.Parsing;
+using System.Linq;
+using YOUR_APPNAME.Services;
+
+namespace YOUR_APPNAME
+{
+    class Program
+    {
+        /// <summary>
+        /// Starts the interactive mode
+        /// </summary>
+        /// <param name="gameExePath">path to the game exe</param>
+        /// <param name="enableMods">optional: enable mod support, by default false </param>
+        /// <returns></returns>
+        static void StartInteractiveMode(string gameExePath, bool enableMods = false)
+        {
+            Console.WriteLine("Initializing InteractiveMode...");
+            
+            if (!WolvenKitWrapper.Initialize(gameExePath, enableMods)) return;
+            
+            Console.WriteLine("App started. Type 'exit' to quit, 'help' for help.");
+            
+            while (true)
+            {
+                Console.Write("> ");
+                string? command = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(command))
+                    continue;
+
+                if (command.ToLower() == "exit")
+                    break;
+
+                HandleCommand(command);
+            }
+        
+            Console.WriteLine("Exiting...");
+        }
+        /// <summary>
+        /// Dispatches / Runs the command in interactive mode
+        /// </summary>
+        /// <param name="command">string of arguments</param>
+        /// <returns></returns>
+        static void HandleCommand(string command)
+        {
+            var commandArray = ParseArguments(command);
+            switch (commandArray[0])
+            {
+                /*
+                 * Add new Commands here
+                 * Make sure to check for argument length before using them
+                 * Add a short description to the help command
+                 * Here WolvenKitWrapper is loaded
+                 * 
+                 * It is best to have little logic here and just call a method
+                 */
+                case "hello":
+                    if (commandArray.Length < 2)
+                    {
+                        Console.WriteLine("This command requires 1 parameter!");
+                        return;
+                    }
+                    Console.WriteLine($"Hello, {commandArray[1]}!");
+                    break;
+                case "help":
+                    Console.WriteLine("Available commands:");
+                    Console.WriteLine("  exit: exit - exits the program");
+                    Console.WriteLine("  help: help - shows this help message");
+                    break;
+                default:
+                    Console.WriteLine("Unknown command.");
+                    break;
+            }
+        }
+        /// <summary>
+        /// Splits the string to an array of arguments
+        /// </summary>
+        /// <param name="input">string to split</param>
+        /// <returns></returns>
+        static string[] ParseArguments(string input)
+        {
+            return CommandLineStringSplitter.Instance.Split(input).ToArray();
+        }
+        
+        /// <summary>
+        /// Method called when app is run
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static void Main(string[] args)
+        { 
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Error: No command provided.");
+                Console.WriteLine("Usage: YOUR_APPNAME.exe <command>");
+                return;
+            }
+            
+            string command = args[0];
+            
+            switch (command)
+            {
+                /*
+                 * This is only for commands outside the interactive mod
+                 * => WolvenKitWrapper is *not* loaded
+                 */
+                case "start":
+                    if (args.Length < 2)
+                    {
+                        Console.WriteLine("start command requires 1 or 2 arguments: <game exe path> optional: <enable mods>");
+                        return;
+                    }
+                    
+                    bool enableMods = false;
+                    if (args.Length > 2)
+                    {
+                        bool.TryParse(args[2], out enableMods);
+                    }
+                    
+                    StartInteractiveMode(args[1], enableMods);
+                    break;
+                case "help":
+                    Console.WriteLine("Available commands:");
+                    Console.WriteLine(" start - starts the interactive mode");
+                    Console.WriteLine(" help - Displays this help message");
+                    break;
+                default:
+                    Console.WriteLine($"Error: Unknown command '{command}'.");
+                    Console.WriteLine("Type 'YOUR_APPNAME.exe help' for a list of available commands.");
+                    break;
+            }
+        }
+    }
+}
---
+++ YOUR_APPNAME/Services/WolvenKitWrapper.cs
@@ -0,0 +1,75 @@
+using System;
+using System.IO;
+using WolvenKit;
+using WolvenKit.Common.Services;
+using WolvenKit.Core.Interfaces;
+using WolvenKit.Core.Services;
+using WolvenKit.RED4.CR2W;
+using WolvenKit.RED4.CR2W.Archive;
+
+namespace YOUR_APPNAME.Services;
+
+public class WolvenKitWrapper
+{
+    private static WolvenKitWrapper? instance;
+
+    private readonly ILoggerService _loggerService;
+    private readonly IProgressService<double> _progressService;
+    
+    public HashService HashService;
+    public HookService HookService;
+    public Red4ParserService Red4ParserService;
+    public ArchiveManager ArchiveManager;
+    public GeometryCacheService GeometryCacheService;
+    
+    private WolvenKitWrapper(string gameExePath, bool enableMods = false)
+    {
+        if (string.IsNullOrEmpty(gameExePath)) throw new ArgumentNullException(nameof(gameExePath), "Game executable path cannot be null or empty.");
+        if (!File.Exists(gameExePath)) throw new FileNotFoundException("Game executable path cannot be found.");
+        
+        _loggerService = new SerilogWrapper();
+        _progressService = new ProgressService<double>();
+        HashService = new HashService();
+        HookService = new HookService();
+        Red4ParserService = new Red4ParserService(HashService, _loggerService, HookService);
+        ArchiveManager = new ArchiveManager(HashService, Red4ParserService, _loggerService, _progressService);
+        
+        ArchiveManager.Initialize(new FileInfo(gameExePath), enableMods);
+        
+        GeometryCacheService = new GeometryCacheService(ArchiveManager, Red4ParserService);
+    }
+
+    /// <summary>
+    /// Initializes the service (should only be called once when interactive mode is being opened), does not return an instance
+    /// </summary>
+    /// <param name="gameExePath"></param>
+    /// <param name="enableMods"></param>
+    /// <returns></returns>
+    public static bool Initialize(string gameExePath, bool enableMods = false)
+    {
+        if (instance != null) return true;
+        try
+        {
+            instance = new WolvenKitWrapper(gameExePath, enableMods);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error initializing WolvenKitWrapper: " + e.Message);
+            return false;
+        }
+    }
+    /// <summary>
+    /// Get the current Instance
+    /// </summary>
+    /// <exception cref="ArgumentException">if wrapper is not initialized</exception>
+    public static WolvenKitWrapper Instance
+    {
+        get
+        {
+            if (instance != null)
+                return instance;
+            throw new ArgumentException("Instance is not initialized");
+        }
+    }
+}
